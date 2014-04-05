fc82358c7b595c083eca091e3d932003138d0e96
Vincent Johnson <vincent_johnson@utexas.edu>
2014-04-04 23:42:13 -0500

    rudimentary player service

---
+++ PingPong.BLL/GameService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PingPong.Data;
+using PingPong.Entities;
+
+namespace PingPong.BLL
+{
+    public class GameService
+    {
+        private readonly IPingPongRepository<Game> _repo;
+        private readonly Game _game;
+
+        public GameService(Game game)
+        {
+            _repo = new PingPongRepository<Game>();
+            _game = game;
+        }
+
+        public IEnumerable<Game> GetGamesByUsername(int playerId)
+        {
+            var games = _repo.FindBy(x => x.ChallengerId == playerId || x.DefenderId == playerId)
+                        .ToList();
+            return games;
+        }
+    }
+}
