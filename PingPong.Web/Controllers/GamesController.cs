a991f329912d85ee5074b08b6657051387776728
Vincent Johnson <vincent_johnson@utexas.edu>
2014-04-04 20:51:25 -0500

    make database work

---
+++ PingPong.Web/Controllers/GamesController.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PingPong.Data;
+using PingPong.Entities;
+
+namespace PingPong.Web.Controllers
+{
+    public class GamesController : Controller
+    {
+        // GET: Games
+        public ActionResult Index()
+        {
+            
+            return View();
+        }
+
+        // GET: Games/Details/5
+        public ActionResult Details(int id)
+        {
+            var repo = new PingPongRepository<Game>();
+            var game = repo.FindBy(x => x.GameId == id).SingleOrDefault();
+            return View(game);
+        }
+
+        // GET: Games/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Games/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Games/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Games/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Games/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Games/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
