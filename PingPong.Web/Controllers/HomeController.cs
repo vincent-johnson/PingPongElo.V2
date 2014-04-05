67678c39a72b4edc6cbfd7b650304579640a3bb1
ebensley <ericb6@uw.edu>
2014-04-04 23:28:54 -0500

    Adding List of users with ELOs to home page

--- PingPong.Web/Controllers/HomeController.cs
+++ PingPong.Web/Controllers/HomeController.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PingPong.BLL;
+using PingPong.Entities;
+using Microsoft.AspNet.Identity;
+using PingPong.Web.Models;
 
 namespace PingPong.Web.Controllers
 {
@@ -10,7 +14,9 @@ namespace PingPong.Web.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            var vM = new HomeIndexViewModel();
+            vM.players = GetPlayers().ToList();
+            return View(vM);
         }
 
         public ActionResult About()
@@ -27,5 +33,10 @@ namespace PingPong.Web.Controllers
             return View();
         }
 
+        public IEnumerable<Player> GetPlayers()
+        { 
+            PlayerService pService = new PlayerService(new Player());
+            return pService.GetAllPlayersRestricted(); //User.Identity.GetUserName();
+        }
     }
 }
