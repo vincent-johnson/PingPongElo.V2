5be05704da265828f6d0772fd1c35861624d427b
Vincent Johnson <vincent_johnson@utexas.edu>
2014-04-09 00:56:09 -0500

    Deleted extra delete method from merge

--- PingPong.Web/Controllers/GamesController.cs
+++ PingPong.Web/Controllers/GamesController.cs
@@ -151,12 +151,6 @@ namespace PingPong.Web.Controllers
             }
         }
 
-        // GET: Games/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
         // POST: Games/Delete/5
         [HttpPost]
         public ActionResult Delete(int id)
